68112318cc2fee0a2de1584a93644cd3af382caf
Arthur Queiroz <arthur.queiroz08@aluno.ifce.edu.br>
2022-08-19 02:20:00 -0300

    Adicionando praticas de data science.

---
+++ Pratica/exercicio11.py
@@ -0,0 +1,17 @@
+#Introdução ao POO
+#Definindo a classe, seus atributos e seus métodos 
+class Quadrado():
+  def __init__(self, lado = 5):
+    self.lado = lado
+  def mostrarlado(self):
+    return self.lado
+  def area(self):
+    return self.lado**2
+  def perimetro(self):
+    return self.lado * 4
+  def setLado(self,novolado):
+    self.lado = novolado
+
+
+qdr = Quadrado(lado = 7)
+print('O lado do quadrado vale ',qdr.lado, '\nsua área vale ', qdr.area(),'\ne seu perimetro vale',qdr.perimetro(),'.')
